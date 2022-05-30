f91c068a6154269c401255b17e561383864864d9
smarbal <35641452+smarbal@users.noreply.github.com>
2022-05-30 11:26:12 +0200

    Update router.js

--- router.js
+++ router.js
@@ -10,11 +10,11 @@ const authController = require('./controllers/authController');
 router.post('/register', userController.userCreate)
 router.post('/login', authController.login)
 
-router.get('/user', authController.isUser, userController.userList);
+router.get('/user', authController.isAdmin, userController.userList);
 router.put('/user/:service_number', authController.isUser, userController.userUpdate);
 router.put('/inside/:service_number', authController.isUser,userController.userUpdateInside);
 router.delete('/user/:service_number', authController.isUser, userController.userDelete); // isAdmin can also be possible
-router.get('/user/find/:service_number', userController.userFindOne)
+router.get('/user/find/:service_number', authController.isUser, userController.userFindOne)
 //router.get('/user/filter', userController.userFindOp);
 //router.get('/user/order', userController.userOrder);
 
